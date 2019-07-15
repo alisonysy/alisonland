f9d65a621dd66f50c49cdfebdc2a20e7779f1bdc
alisonys <qwen735@aucklanduni.ac.nz>
2019-07-15 22:35:32 +0800

    menuSlider init

--- src/components/menuSlider.vue
+++ src/components/menuSlider.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="slider-wrapper">
     <ul>
-      
+      <li v-for="item in slideItems" >
+        <router-link>
+          {{item}}
+        </router-link>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name:'menu-slider'
+  name:'menu-slider',
+  props:['lis'],
+  data:function(){
+    return {
+      slideItems:this.lis
+    }
+  }
 }
 </script>
 
